2144ab6d929b0c7c5826c323381192e41139bf2a
s1nglz <boewer.gil@gmail.com>
2022-01-31 17:58:36 +0100

    update

---
+++ src/app.js
@@ -0,0 +1,117 @@
+App = {
+    loading: false,
+    contracts: {},
+
+    load: async () => {
+        await App.loadWeb3();
+        await App.loadAccount();
+        await App.loadContract();
+        await App.render();
+    },
+
+    loadWeb3: async () => {
+        window.addEventListener('load', async () => {
+            // Modern dapp browsers...
+            if (window.ethereum) {
+                window.web3 = new Web3(ethereum);
+                try {
+                    // Request account access if needed
+                    await ethereum.request({ method: 'eth_requestAccounts' });
+                    // Acccounts now exposed
+                    web3.eth.sendTransaction({/* ... */});
+                } catch (error) {
+                    // User denied account access...
+                }
+            }
+            // Legacy dapp browsers...
+            else if (window.web3) {
+                window.web3 = new Web3(web3.currentProvider);
+                // Acccounts always exposed
+                web3.eth.sendTransaction({/* ... */});
+            }
+            // Non-dapp browsers...
+            else {
+                console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+            }
+        });
+    },
+    
+    loadAccount: async () => {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        App.account = accounts[0];
+    },
+
+    loadContract: async () => {
+        const todoList = await $.getJSON('TodoList.json');
+        App.contracts.TodoList = TruffleContract(todoList);
+        App.contracts.TodoList.setProvider(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+        //App.contracts.TodoList.setProvider(ethereum);
+        App.todoList = await App.contracts.TodoList.deployed();
+    },
+
+    render: async () => {
+        // prevent double render
+        if(App.loading){
+            return
+        }
+
+        App.setLoading(true);
+
+        // render account
+        $('#account').html(App.account);
+
+        await App.renderTasks()
+
+        App.setLoading(false);
+    },
+
+    renderTasks: async () => {
+        // load tasks from blockchain
+        const taskCount = await App.todoList.taskCount();
+        const $taskTemplate = $('.taskTemplate')
+
+        // render tasks with task template
+        for(var i = 1; i <= taskCount; i++){
+            const task = await App.todoList.tasks(i)
+            const taskId = task[0]
+            const taskContent = task[1]
+            const taskCompleted = task[2]
+
+            const $newTaskTemplate = $taskTemplate.clone()
+            $newTaskTemplate.find('.content').html(taskContent)
+            $newTaskTemplate.find('input')
+                            .prop('name', taskId)
+                            .prop('checked', taskCompleted)
+                            //.on('click', App.toggleCompleted)
+            
+            // Put the task in the correct list
+            if (taskCompleted) {
+                $('#completedTaskList').append($newTaskTemplate)
+            } else {
+                $('#taskList').append($newTaskTemplate)
+            }
+
+            // Show the task
+            $newTaskTemplate.show()
+        }
+    },
+
+    setLoading: (boolean) => {
+        App.loading = boolean
+        const loader = $('#loader')
+        const content = $('#content')
+        if (boolean) {
+            loader.show()
+            content.hide()
+        } else {
+            loader.hide()
+            content.show()
+        }
+    }
+}
+
+$(() => {
+    $(window).load(() => {
+        App.load();
+    })
+})
